f73e004137aef7569c006e24563f8fd280ce75d0
unknown <najmulcse247@gmail.com>
2024-09-30 08:50:49 +0600

    toggle learn

--- src/components/Services/Service.jsx
+++ src/components/Services/Service.jsx
@@ -1,7 +1,16 @@
 
 const Service = () => {
+  let toggle = false;
+  function changeName(){
+    const name = document.getElementById('changeName');
+    toggle =!toggle;
+    name.innerText=toggle?'Najmul':'Hasan';
+  }
   return (
-    <div>Service</div>
+    <div>
+      <h1>This is <span id="changeName">Najmul</span></h1>
+      <button onClick={changeName}>Change Name</button>
+    </div>
   )
 }
 
